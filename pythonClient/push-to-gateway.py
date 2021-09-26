8de865ae21accbc39e5a99ee879ce4cc8c0207fa
aussiearef <arefkr@gmail.com>
2021-09-26 16:38:38 +1000

    added example cert files

--- pythonClient/push-to-gateway.py
+++ pythonClient/push-to-gateway.py
@@ -1,4 +1,9 @@
-from prometheus_client import push_to_gateway, CollectorRegistry, Gauge, registry
+from prometheus_client import push_to_gateway, CollectorRegistry, Gauge, registry, utils
+from prometheus_client import exposition
+from prometheus_client.exposition import basic_auth_handler
+
+def auth_handler(url, method, timeout, headers, data):
+   return basic_auth_handler(url, method, timeout, headers, data, "admin", "password")
 
 registry = CollectorRegistry()
 
@@ -6,4 +11,4 @@ gauge = Gauge("python_push_to_gateway", "python_push_to_gateway", registry= regi
 
 while True:
     gauge.set_to_current_time()
-    push_to_gateway("localhost:9091", job="Job A", registry = registry)
+    push_to_gateway("localhost:9091", job="Job A", registry = registry, handler=auth_handler)
